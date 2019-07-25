379b6deb077731c0196dd90f43aba8362457af23
Sainingo <nickmentor421@gmail.com>
2019-07-25 22:28:38 +0300

    animated site

---
+++ js/demo.js
@@ -0,0 +1,41 @@
+/**
+ * Particleground demo
+ * @author Jonathan Nicol - @mrjnicol
+ */
+
+// This can be used to set the Particles Effects. Check README for more details!
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+} 
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
+
